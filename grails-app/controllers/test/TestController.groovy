2b70134a9ddf16125875667083dd12ea8779d493
Mauro Ciancio <mauro.ciancio@cymait.com>
2018-10-07 19:50:27 -0300

    remove unneeded

--- grails-app/controllers/test/TestController.groovy
+++ grails-app/controllers/test/TestController.groovy
@@ -3,10 +3,9 @@ package test
 class TestController {
 
     def get() {
-      [params: params, model: request.algo]
     }
 
     def post() {
-      forward action: 'get', model: [algo: true]
+      forward action: 'get'
     }
 }
